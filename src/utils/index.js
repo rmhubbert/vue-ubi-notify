001c6dc8414c46b2d2444e141cb29121fded900f
Hubby <hubby@rmhubbert.com>
2019-08-09 17:33:18 +0100

    Remove obsolete function

--- src/utils/index.js
+++ src/utils/index.js
@@ -30,46 +30,6 @@ const Util = {
     return (
       target && typeof target === "object" && target.constructor === Object
     );
-  },
-
-  /**
-   * Constructs a valid CSS positioning string from a config string
-   * @param  {String} positionString Config provided string that can be either exact position or keywords
-   * @param  {String} elementWidth   Needed for when the horizontal value is 'center'
-   * @return {String}                A valid CSS string for adding to a style attribute
-   */
-  getCSSPosition: (positionString, elementWidth) => {
-    if (positionString.search(":") !== -1) {
-      // user has passed a precise position, return as is
-      return positionString;
-    } else {
-      const validFirstWords = ["top", "bottom"];
-      const validSecondWords = ["left", "right", "center"];
-      const words = positionString.split(" ");
-      if (words.length >= 2) {
-        let hPos = null;
-        let vPos = null;
-        const firstWord = words[0].trim().toLowerCase();
-        const secondWord = words[1].trim().toLowerCase();
-
-        if (validFirstWords.indexOf(firstWord) > -1) {
-          vPos = `${firstWord}: 0;`;
-        }
-        if (validSecondWords.indexOf(secondWord) > -1) {
-          if (secondWord === "center") {
-            let numericWidth = elementWidth.match(/\d+/g);
-            let widthType = elementWidth.replace(/[0-9]/g, "");
-            hPos = `left: 50%; margin-left: -${numericWidth / 2}${widthType};`;
-          } else hPos = `${secondWord}: 0;`;
-        }
-
-        if (hPos && vPos) {
-          return `${vPos} ${hPos}`;
-        } else if (hPos) return `top: 0; ${hPos}`;
-        else if (vPos) return `${vPos} right: 0;`;
-      }
-    }
-    return `top: 0; right: 0;`;
   }
 };
 
